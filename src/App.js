6e803798d3bbd84d13883a502e0fcb8e0f6f5b97
npx <shravankumar@lyrostech.com>
2024-03-20 12:33:57 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import { useState, createContext, useContext } from "react";
-import Login from "./Login";
-import Register from "./Register";
-import Home from "./Home";
-export const UserContext = createContext();
+import React, { useContext } from "react";
+// import { UserContext } from '../App'
+import { Usecontext1 } from "./UserContext/Usercontext1";
+//import { srikanth } from '../UserContext/Usercontext1';
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+
+import Feeds from "./Components/Feeds";
+import Posts from "./Components/Posts";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Albums from "./Components/Albums";
+import Comments from "./Components/Comments";
+
 export default function App() {
-  const [user, setUser] = useState("John");
-  const [flag, setFlag] = useState(0);
- 
+  const { flag, setFlag } = useContext(Usecontext1);
+  //const {flag,setFlag} = useContext(srikanth)
+  //const [flag,setFlag] = useState()
   return (
     <>
-      <UserContext.Provider value={{ user, setUser, flag, setFlag } }>
-       
-        {flag==0||flag==1?<Login/>:<Home/>}
-     
-      </UserContext.Provider>
+    <Router>
+      {flag == 0 || flag == 2 ? <Login /> : <Home />}
+      
+      {flag === 2 && <Register />}
+
+      
+        <Routes>
+          <Route path="/" index element={<Feeds />}></Route>
+          <Route path="/post" element={<Posts />}></Route>
+          <Route path="/albums" element={<Albums />}></Route>
+          <Route path="/comments" element={<Comments/>}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
